2ac0694db7fc84da1d58a41edad7afddf71fdf84
Ben <benmason1992@hotmail.co.uk>
2020-03-23 15:13:26 +0000

    created first table in migration

---
+++ database/migrations/2020_03_23_151146_create_cocktails_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCocktailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cocktails', function (Blueprint $table) {
+            $table->id();
+            $table->string("name", 100);
+            $table->text("method");
+            $table->string("image", 100);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cocktails');
+    }
+}
