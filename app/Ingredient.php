2e7927b5c3e61b4189c28be667557b155fd07d8d
Oli Ward <oliward@gmail.com>
2020-03-24 16:34:57 +0000

    cleanup conflict tag

--- app/Ingredient.php
+++ app/Ingredient.php
@@ -8,11 +8,7 @@ use App\Cocktail;
 class Ingredient extends Model
 {
     public $timestamps = false;
-<<<<<<< HEAD
-    
-=======
 
->>>>>>> 391d356d58488aa28f9a350732a7ec2330f081c1
     public function cocktails() {
         return $this->belongsToMany(Cocktail::class);
     }
