4e1a48588401f932ba345ab4778903f7ce4b745b
vagrant <vagrant@mixr-api>
2020-03-24 12:15:01 +0000

    adding the pivot table

--- app/Ingredient.php
+++ app/Ingredient.php
@@ -3,8 +3,13 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Cocktail;
 
 class Ingredient extends Model
 {
-    //
+    public $timestamps = false;
+    
+    public function ingredients() {
+        return $this->belongsToMany(Cocktail::class);
+    }
 }
