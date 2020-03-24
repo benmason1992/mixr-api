3f4b38cc8e65e5cc4e12c269828200b2bccd4209
vagrant <vagrant@mixr-api>
2020-03-24 16:24:23 +0000

    commit for debugging

--- routes/api.php
+++ routes/api.php
@@ -1,18 +1,22 @@
 <?php
 
 namespace App\Http\Controllers\API;
-use App\Http\Requests\API\CocktailRequest;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\Cocktails;
+use App\Http\Requests\API\CocktailRequest;
+
+
 
 Route::group(["prefix" => "cocktails"], function () {
     Route::get("", [Cocktails::class, "index"]);
+    Route::post("", [Cocktails::class, "store"]);
 
     Route::group(["prefix" => "{cocktail}"], function () {
         Route::get("", [Cocktails::class, "show"]);
         Route::put("", [Cocktails::class, "update"]);
+        Route::delete("", [Cocktails::class, "destroy"]);    
     });
 
 });
