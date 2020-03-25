f2a06521c734c9256542191e10e656cfb38ae16f
Rebeka Meszaros <akeber.m@live.com>
2020-03-25 10:32:27 +0000

    updates

--- app/Http/Controllers/API/Ingredients.php
+++ app/Http/Controllers/API/Ingredients.php
@@ -1,8 +1,12 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\API;
 
 use Illuminate\Http\Request;
+
+use App\Http\Controllers\Controller;
+use App\Ingredient;
+
 
 class Ingredients extends Controller
 {
